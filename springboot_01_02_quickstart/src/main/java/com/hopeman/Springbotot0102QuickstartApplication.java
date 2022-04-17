e3d754a720f30510ec570a4d5a2643c4fc115052
Hopeman <528419003@qq.com>
2022-04-17 21:26:38 +0800

    springboot快速入门

---
+++ springboot_01_02_quickstart/src/main/java/com/hopeman/Springbotot0102QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.hopeman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbotot0102QuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbotot0102QuickstartApplication.class, args);
+	}
+
+}
